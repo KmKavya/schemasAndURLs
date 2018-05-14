abd8efa1b4a39674dd3da1cf898e89b8cee49e14
KmKavya <kavyavshaiva@gmail.com>
2018-05-14 11:30:46 +0530

    validation Tests

---
+++ validationTest/role.test.js
@@ -0,0 +1,54 @@
+const chai = require("chai");
+const expect = chai.expect;
+
+const validate = require("jsonschema").validate;
+const roleSchema = require('../roleSchema').schema;
+
+let validRole = {
+  roleId: 1,
+  roleName: "admin",
+  createdBy: "system",
+  createdDate: new Date().toISOString()
+};
+
+describe("testing role schema", () => {
+  it('should validate role schema', (done) => {
+    var res = validate(validRole, roleSchema);
+    expect(res.valid).to.deep.eql(true);
+    done();
+  });
+
+  it('should not validate role schema', (done) => {
+    var res = validate({
+      roleId: "k",
+      roleName: "admin",
+      createdBy: "system",
+      createdDate: new Date().toISOString()
+    }, roleSchema);
+    expect(res.valid).to.deep.eql(false);
+    done()
+  });
+
+  it('should return requires property error', (done) => {
+    var res = validate({
+      roleId: 1,
+      roleName: "admin",
+      createdBy: "system"
+    }, roleSchema);
+    expect(res.errors[0].message).to.eql('requires property "createdDate"');
+    done();
+  });
+
+  it('should return error for undefined roleName input', (done) => {
+    var k;
+    var res = validate({
+      roleId: 1,
+      roleName: k,
+      createdBy: "system",
+      createdDate: new Date().toISOString()
+    }, roleSchema);
+    expect(res.errors[0].message).to.eql('requires property "roleName"');
+    done();
+  });
+
+});
